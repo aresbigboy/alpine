c38d20d0ee7e35714a19bf71f71e6ffbda858180
aresbigboy <wei.ares@gmail.com>
2020-05-26 19:42:44 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+curl -L -H "Cache-Control: no-cache" -o ./v2ray.zip https://github.com/v2ray/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+mkdir -p ./v2ray/
+unzip ./v2ray.zip -d ./v2ray/
+rm -rf ./v2ray.zip ./v2ray/*.sig ./v2ray/doc ./v2ray/*.json ./v2ray/*.dat ./v2ray/sys*
+chmod -R +x ./v2ray
+
 cat <<-EOF > ./v2ray/config.json
 $CONFIG_JSON
 EOF
