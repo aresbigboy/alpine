614c56ceb39f90d427675821c9fa29959961f80f
aresbigboy <wei.ares@gmail.com>
2020-05-26 19:37:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # V2Ray new configuration
-cat <<-EOF > /etc/v2ray/config.json
+cat <<-EOF > ./config.json
 $CONFIG_JSON
 EOF
 # Run V2Ray
-/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
+./v2ray/v2ray -config=./config.json
